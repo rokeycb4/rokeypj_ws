ca0711f38c0e8d9f4fcc71a53e7eaffcedcbca5b
kiwi_ub <mingureion@gmail.com>
2025-06-17 14:54:26 +0900

    chore: 파일명 정리

--- src/turtlebot3_autorace_camera/launch/extrinsic_camera_calibration2.launch.py
+++ src/turtlebot3_autorace_camera/launch/extrinsic_camera_calibration2.launch.py
@@ -1,22 +1,3 @@
-## 1차로 수정한거
-#!/usr/bin/env python3
-#
-# Copyright 2025 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Hyungyu Kim
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -25,12 +6,11 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     calibration_mode_arg = DeclareLaunchArgument(
         'calibration_mode',
         default_value='False',
-        description='calibration mode type [Ture, False]')
+        description='calibration mode type [True, False]')
     calibration_mode = LaunchConfiguration('calibration_mode')
 
     compensation_param = os.path.join(
@@ -38,14 +18,14 @@ def generate_launch_description():
         'calibration',
         'extrinsic_calibration',
         'compensation.yaml'
-        )
+    )
 
     projection_param = os.path.join(
         get_package_share_directory('turtlebot3_autorace_camera'),
         'calibration',
         'extrinsic_calibration',
         'projection.yaml'
-        )
+    )
 
     image_projection_node = Node(
         package='turtlebot3_autorace_camera',
@@ -58,16 +38,12 @@ def generate_launch_description():
             {'is_extrinsic_camera_calibration_mode': calibration_mode}
         ],
         remappings=[
-            ('/camera/image_input', '/image_raw'),
-            ('/camera/image_input/compressed', '/image_raw/compressed'),
-
+            ('/camera/image_input', '/camera/image'),
             ('/camera/image_output', '/camera/image_projected'),
             ('/camera/image_output/compressed', '/camera/image_projected/compressed'),
-
             ('/camera/image_calib', '/camera/image_extrinsic_calib'),
             ('/camera/image_calib/compressed', '/camera/image_extrinsic_calib/compressed')
         ],
-
     )
 
     image_compensation_node = Node(
@@ -78,16 +54,16 @@ def generate_launch_description():
         output='screen',
         parameters=[{
             'is_extrinsic_camera_calibration_mode': calibration_mode
-            },
-            compensation_param
-        ],
-        remappings=[
-            ('/camera/image_input', '/image_raw'),
-            ('/camera/image_input/compressed', '/image_raw/compressed'),
-            
-            ('/camera/image_output', '/camera/image_compensated'),
-            ('/camera/image_output/compressed', '/camera/image_compensated/compressed')
-        ]
+        }, compensation_param],
+
+    remappings=[
+        ('/camera/image_input', '/camera/image'),
+        ('/camera/image_input/compressed', '/camera/image'),
+        
+        ('/camera/image_output', '/camera/image_compensated'),
+        ('/camera/image_output/compressed', '/camera/image_compensated/compressed')
+    ]
+
     )
 
     return LaunchDescription([
