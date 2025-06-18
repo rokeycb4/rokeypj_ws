75afaea2125c0af30ed7da31fb9d14972c604d1d
rokeycb4 <rokeybcb4@gmail.com>
2025-06-18 14:46:17 +0900

    aruco

--- src/aruco_yolo/setup.py
+++ src/aruco_yolo/setup.py
@@ -28,6 +28,7 @@ setup(
             'aruco_move = aruco_yolo.aruco_move:main',
             'compressed_image_pub = aruco_yolo.compressed_image_pub:main',
             'yolo_detector = aruco_yolo.yolo_detector:main',
+            'turtlebot_aruco = aruco_yolo.turtlebot_aruco:main'
         ],
     },
     package_data={
